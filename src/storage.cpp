2744a4d2746fb3096265c83cc999f54fb7be4912
WijayaTimothyLarry <wijayatimothylarry@gmail.com>
2023-09-13 20:38:29 +0800

    added storage class

---
+++ src/storage.cpp
@@ -0,0 +1,37 @@
+#include "storage.h"
+#include <cstring>
+
+using namespace std;
+
+Storage::Storage(unsigned int storageSize, unsigned int blockSize) {
+    this->storageSize = storageSize;
+    this->blockSize = blockSize;
+
+    storagePtr = new unsigned char[storageSize];
+    currBlockPtr = nullptr;
+
+    numOfBlocks = storageSize / blockSize;
+    numOfAllocatedBlocks = 0;
+    currBlockUsedSpace = 0;
+}
+
+Storage::~Storage() {
+    delete storagePtr;
+}
+
+void *Storage::storeRecord(NBARecord record) {
+    if(blockSize - currBlockUsedSpace <= sizeof(record) || numOfAllocatedBlocks == 0){
+        if(numOfAllocatedBlocks > 0 ){
+            currBlockPtr = storagePtr + numOfAllocatedBlocks * blockSize;
+            numOfAllocatedBlocks += 1;
+            currBlockUsedSpace = 0;
+        } else {
+            throw "There are no available space";
+        }
+    }
+
+    void *recordPtr = currBlockPtr + currBlockUsedSpace;
+    currBlockUsedSpace += sizeof(record);
+    memcpy(recordPtr, &record, sizeof(record));
+    return recordPtr;
+}
---
+++ src/storage.h
@@ -0,0 +1,35 @@
+#ifndef B_PLUS_TREE_PROJECT_STORAGE_H
+#define B_PLUS_TREE_PROJECT_STORAGE_H
+
+const unsigned int blockSize = 400;
+
+struct NBARecord{
+    unsigned int teamID; //4 bytes
+    unsigned int homePoints; //4 bytes
+    float homeFGPercentage; //4 bytes
+    float homeFTPercentage; //4 bytes
+    float homeFG3Percentage; //4 bytes
+    unsigned int homeAssist; //4 bytes
+    unsigned int homeRebound; //4 bytes
+    bool homeTeamWins; //1 byte
+};
+
+class Storage{
+private:
+    unsigned char *storagePtr;
+    unsigned char *currBlockPtr;
+
+    unsigned int storageSize;
+    unsigned int blockSize;
+    unsigned int numOfBlocks;
+    unsigned int numOfAllocatedBlocks;
+    unsigned int currBlockUsedSpace;
+
+public:
+    Storage(unsigned int storageSize, unsigned int blockSize);
+
+    ~Storage();
+
+    void* storeRecord(NBARecord record);
+};
+#endif //B_PLUS_TREE_PROJECT_STORAGE_H
