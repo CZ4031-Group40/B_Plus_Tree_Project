e3cd8a72ef6dbb8eb5669c4d9eb36da37aa1f24d
AmosChong20 <95435362+AmosChong20@users.noreply.github.com>
2023-09-21 23:19:09 +0800

    Search for a key+Display (#7)
    * Search record and display Tree
    
    * Modified Initialisation
    
    * Minor modifications with Larry's suggestions

--- src/BPlusTree.cpp
+++ src/BPlusTree.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <queue>
 #include "BPlusTree.h"
 #include "Storage.h"
 
@@ -109,19 +110,19 @@ BPlusTree::BPlusTree(vector<tuple<float, void *>> &initialData) {
             BPNode *lastNode = parentNodes[parentNodes.size()-1];
             int elementsToMove = lastNode->keys.size() - (lastNode->keys.size() + parentNode->keys.size()) / 2;
 
-            parentNode->keys.insert(
-                    parentNode->keys.begin(),
-                    lastNode->keys.end() - elementsToMove,
-                    lastNode->keys.end()
-            );
             parentNode->childNodePtrs.insert(
                     parentNode->childNodePtrs.begin(),
-                    lastNode->childNodePtrs.end() - elementsToMove-1,
+                    lastNode->childNodePtrs.end() - elementsToMove,
                     lastNode->childNodePtrs.end()
             );
+            for (unsigned int i = elementsToMove; i > 0; i--) {
+                parentNode->keys.insert(
+                        parentNode->keys.begin(),
+                        parentNode->childNodePtrs[i]->minKey
+                );
+            }
             parentNode->minKey = parentNode->childNodePtrs[0]->minKey;
-
-            lastNode->keys.resize(lastNode->keys.size() - elementsToMove - 1);
+            lastNode->keys.resize(lastNode->keys.size() - elementsToMove);
             lastNode->childNodePtrs.resize(lastNode->childNodePtrs.size() - elementsToMove);
         }
 
@@ -140,8 +141,68 @@ void BPlusTree::insertRecord() {
 
 }
 
-void* BPlusTree::searchRecord(int key) {
- return nullptr;
+NBARecords *BPlusTree::searchRecord(float key) {
+    if (root == nullptr) {
+        return nullptr;
+    }
+    BPNode *current = root;
+    while (!current->isLeaf) {
+        for (int i = 0; i < current->keys.size(); i++) {
+            if (key < current->keys[i]) {
+                current = current->childNodePtrs[i];
+                break;
+            }
+
+            if (i == current->keys.size()-1) {
+                current = current->childNodePtrs[i+1];
+                break;
+            }
+        }
+    }
+
+    for (int i = 0; i < current->keys.size(); i++) {
+        if (key == current->keys[i]) {
+            return current->recordPtrs[i];
+        }
+    }
+
+    return nullptr;
+}
+
+void BPlusTree::displayTree(BPNode *current) {
+    if (current == nullptr) {
+        return;
+    }
+    int level = 1;
+    queue<BPNode *> q;
+    q.push(current);
+
+    while (!q.empty()) {
+        int l;
+        l = q.size();
+        cout << "Level " << level++ << ": ";
+
+        for (int i = 0; i < l; i++) {
+            BPNode *node = q.front();
+            q.pop();
+
+            for (int j = 0; j < node->keys.size(); j++) {
+                cout << node->keys[j] << " ";
+            }
+
+            for (int j = 0; j < node->childNodePtrs.size(); j++) {
+                if (node->childNodePtrs[j] != nullptr) {
+                    q.push(node->childNodePtrs[j]);
+                }
+            }
+
+            cout << "\t";
+        }
+        cout << endl;
+        cout << endl;
+    }
+
+
 }
 
 void BPlusTree::deleteRecord() {
